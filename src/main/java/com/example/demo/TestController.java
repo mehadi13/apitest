5680431c96a335918ff44b73178c2a874aaf987b
Mehadi Hossain <mehadi_hossain@yahoo.com>
2025-07-23 10:08:01 +0600

    first commit

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    // This is a simple test controller
+    // You can add request mapping methods here to handle HTTP requests
+    // For example:
+
+     @GetMapping("/test")
+     public String testEndpoint() {
+         return "This is a test endpoint";
+     }
+}
